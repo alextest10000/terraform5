8dafb4628dd38893d014a94cfce44cfc4a20f1a1
alextest10000 <53318382+alextest10000@users.noreply.github.com>
2019-08-23 11:46:48 -0700

    Update main.tf

--- exercise_3/main.tf
+++ exercise_3/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 terraform {
    backend "s3" {
-   bucket = "rc-tf-remote-state-bucket"
+   bucket = "rc-tf-bucket-training-exersize-alex2"
      #key is already removed
    region = "us-east-1"
   }
